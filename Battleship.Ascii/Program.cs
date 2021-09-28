f971a7adb9bd7a9a2fb2eae85c12e2a9776eee08
ThomasSchissler <tschissler@agilemax.de>
2021-09-28 20:20:42 +0200

    Added basic telemetry data

--- Battleship.Ascii/Program.cs
+++ Battleship.Ascii/Program.cs
@@ -1,168 +1,227 @@
 ﻿
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Threading;
+
 namespace Battleship.Ascii
 {
-   using System;
-   using System.Collections.Generic;
-   using System.Linq;
+    using Battleship.GameController;
+    using Battleship.GameController.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-   using Battleship.GameController;
-   using Battleship.GameController.Contracts;
+    internal class Program
+    {
+        private static List<Ship> myFleet;
 
-   internal class Program
-   {
-      private static List<Ship> myFleet;
+        private static List<Ship> enemyFleet;
 
-      private static List<Ship> enemyFleet;
+        private static TelemetryClient telemetryClient;
 
-      static void Main()
-      {
-         Console.WriteLine("                                     |__");
-         Console.WriteLine(@"                                     |\/");
-         Console.WriteLine("                                     ---");
-         Console.WriteLine("                                     / | [");
-         Console.WriteLine("                              !      | |||");
-         Console.WriteLine("                            _/|     _/|-++'");
-         Console.WriteLine("                        +  +--|    |--|--|_ |-");
-         Console.WriteLine(@"                     { /|__|  |/\__|  |--- |||__/");
-         Console.WriteLine(@"                    +---------------___[}-_===_.'____                 /\");
-         Console.WriteLine(@"                ____`-' ||___-{]_| _[}-  |     |_[___\==--            \/   _");
-         Console.WriteLine(@" __..._____--==/___]_|__|_____________________________[___\==--____,------' .7");
-         Console.WriteLine(@"|                        Welcome to Battleship                         BB-61/");
-         Console.WriteLine(@" \_________________________________________________________________________|");
-         Console.WriteLine();
+        ////////////////////////////////////////////////
+        // Please replace TeamA with your team name here
+        private static string teamName = "TeamA";
 
-         InitializeGame();
+        static void Main()
+        {
+            InitializeTelemetryClient();
 
-         StartGame();
-      }
-
-      private static void StartGame()
-      {
-         Console.Clear();
-         Console.WriteLine("                  __");
-         Console.WriteLine(@"                 /  \");
-         Console.WriteLine("           .-.  |    |");
-         Console.WriteLine(@"   *    _.-'  \  \__/");
-         Console.WriteLine(@"    \.-'       \");
-         Console.WriteLine("   /          _/");
-         Console.WriteLine(@"  |      _  /""");
-         Console.WriteLine(@"  |     /_\'");
-         Console.WriteLine(@"   \    \_/");
-         Console.WriteLine(@"    """"""""");
-
-         do
-         {
-            Console.WriteLine();
-            Console.WriteLine("Player, it's your turn");
-            Console.WriteLine("Enter coordinates for your shot :");
-            var position = ParsePosition(Console.ReadLine());
-            var isHit = GameController.CheckIsHit(enemyFleet, position);
-            if (isHit)
+            try
             {
-               Console.Beep();
+                TrackTelemetryEvent("ApplicationStarted");
 
-               Console.WriteLine(@"                \         .  ./");
-               Console.WriteLine(@"              \      .:"";'.:..""   /");
-               Console.WriteLine(@"                  (M^^.^~~:.'"").");
-               Console.WriteLine(@"            -   (/  .    . . \ \)  -");
-               Console.WriteLine(@"               ((| :. ~ ^  :. .|))");
-               Console.WriteLine(@"            -   (\- |  \ /  |  /)  -");
-               Console.WriteLine(@"                 -\  \     /  /-");
-               Console.WriteLine(@"                   \  \   /  /");
+                //throw new NotImplementedException();
+
+                Console.WriteLine("                                     |__");
+                Console.WriteLine(@"                                     |\/");
+                Console.WriteLine("                                     ---");
+                Console.WriteLine("                                     / | [");
+                Console.WriteLine("                              !      | |||");
+                Console.WriteLine("                            _/|     _/|-++'");
+                Console.WriteLine("                        +  +--|    |--|--|_ |-");
+                Console.WriteLine(@"                     { /|__|  |/\__|  |--- |||__/");
+                Console.WriteLine(@"                    +---------------___[}-_===_.'____                 /\");
+                Console.WriteLine(@"                ____`-' ||___-{]_| _[}-  |     |_[___\==--            \/   _");
+                Console.WriteLine(@" __..._____--==/___]_|__|_____________________________[___\==--____,------' .7");
+                Console.WriteLine(@"|                        Welcome to Battleship                         BB-61/");
+                Console.WriteLine(@" \_________________________________________________________________________|");
+                Console.WriteLine();
+
+                InitializeGame();
+
+                StartGame();
+            }
+            catch (Exception e)
+            {
+                TrackTelemetryException(e);
+                telemetryClient.Flush();
+                Console.WriteLine(e);
+
+                Thread.Sleep(5000);
             }
 
-            Console.WriteLine(isHit ? "Yeah ! Nice hit !" : "Miss");
+        }
 
-            position = GetRandomPosition();
-            isHit = GameController.CheckIsHit(myFleet, position);
-            Console.WriteLine();
-            Console.WriteLine("Computer shot in {0}{1} and {2}", position.Column, position.Row, isHit ? "has hit your ship !" : "miss");
-            if (isHit)
+        private static void StartGame()
+        {
+            Console.Clear();
+            Console.WriteLine("                  __");
+            Console.WriteLine(@"                 /  \");
+            Console.WriteLine("           .-.  |    |");
+            Console.WriteLine(@"   *    _.-'  \  \__/");
+            Console.WriteLine(@"    \.-'       \");
+            Console.WriteLine("   /          _/");
+            Console.WriteLine(@"  |      _  /""");
+            Console.WriteLine(@"  |     /_\'");
+            Console.WriteLine(@"   \    \_/");
+            Console.WriteLine(@"    """"""""");
+
+            do
             {
-               Console.Beep();
+                Console.WriteLine();
+                Console.WriteLine("Player, it's your turn");
+                Console.WriteLine("Enter coordinates for your shot :");
+                var position = ParsePosition(Console.ReadLine());
+                var isHit = GameController.CheckIsHit(enemyFleet, position);
+                if (isHit)
+                {
+                    Console.Beep();
 
-               Console.WriteLine(@"                \         .  ./");
-               Console.WriteLine(@"              \      .:"";'.:..""   /");
-               Console.WriteLine(@"                  (M^^.^~~:.'"").");
-               Console.WriteLine(@"            -   (/  .    . . \ \)  -");
-               Console.WriteLine(@"               ((| :. ~ ^  :. .|))");
-               Console.WriteLine(@"            -   (\- |  \ /  |  /)  -");
-               Console.WriteLine(@"                 -\  \     /  /-");
-               Console.WriteLine(@"                   \  \   /  /");
+                    Console.WriteLine(@"                \         .  ./");
+                    Console.WriteLine(@"              \      .:"";'.:..""   /");
+                    Console.WriteLine(@"                  (M^^.^~~:.'"").");
+                    Console.WriteLine(@"            -   (/  .    . . \ \)  -");
+                    Console.WriteLine(@"               ((| :. ~ ^  :. .|))");
+                    Console.WriteLine(@"            -   (\- |  \ /  |  /)  -");
+                    Console.WriteLine(@"                 -\  \     /  /-");
+                    Console.WriteLine(@"                   \  \   /  /");
+                }
 
+                Console.WriteLine(isHit ? "Yeah ! Nice hit !" : "Miss");
+
+                position = GetRandomPosition();
+                isHit = GameController.CheckIsHit(myFleet, position);
+                Console.WriteLine();
+                Console.WriteLine("Computer shot in {0}{1} and {2}", position.Column, position.Row, isHit ? "has hit your ship !" : "miss");
+                if (isHit)
+                {
+                    Console.Beep();
+
+                    Console.WriteLine(@"                \         .  ./");
+                    Console.WriteLine(@"              \      .:"";'.:..""   /");
+                    Console.WriteLine(@"                  (M^^.^~~:.'"").");
+                    Console.WriteLine(@"            -   (/  .    . . \ \)  -");
+                    Console.WriteLine(@"               ((| :. ~ ^  :. .|))");
+                    Console.WriteLine(@"            -   (\- |  \ /  |  /)  -");
+                    Console.WriteLine(@"                 -\  \     /  /-");
+                    Console.WriteLine(@"                   \  \   /  /");
+
+                }
             }
-         }
-         while (true);
-      }
+            while (true);
+        }
 
-      internal static Position ParsePosition(string input)
-      {
-         var letter = (Letters)Enum.Parse(typeof(Letters), input.ToUpper().Substring(0, 1));
-         var number = int.Parse(input.Substring(1, 1));
-         return new Position(letter, number);
-      }
+        internal static Position ParsePosition(string input)
+        {
+            var letter = (Letters)Enum.Parse(typeof(Letters), input.ToUpper().Substring(0, 1));
+            var number = int.Parse(input.Substring(1, 1));
+            return new Position(letter, number);
+        }
 
-      private static Position GetRandomPosition()
-      {
-          int rows = 8;
-          int lines = 8;
-          var random = new Random();
-          var letter = (Letters)random.Next(lines);
-          var number = random.Next(rows);
-          var position = new Position(letter, number);
-          return position;
-      }
+        private static Position GetRandomPosition()
+        {
+            int rows = 8;
+            int lines = 8;
+            var random = new Random();
+            var letter = (Letters)random.Next(lines);
+            var number = random.Next(rows);
+            var position = new Position(letter, number);
+            return position;
+        }
 
-      private static void InitializeGame()
-      {
-         InitializeMyFleet();
+        private static void InitializeGame()
+        {
+            InitializeMyFleet();
 
-         InitializeEnemyFleet();
-      }
+            InitializeEnemyFleet();
+        }
 
-      private static void InitializeMyFleet()
-      {
-         myFleet = GameController.InitializeShips().ToList();
+        private static void InitializeMyFleet()
+        {
+            myFleet = GameController.InitializeShips().ToList();
 
-         Console.WriteLine("Please position your fleet (Game board size is from A to H and 1 to 8) :");
+            Console.WriteLine("Please position your fleet (Game board size is from A to H and 1 to 8) :");
 
-         foreach (var ship in myFleet)
-         {
-            Console.WriteLine();
-            Console.WriteLine("Please enter the positions for the {0} (size: {1})", ship.Name, ship.Size);
-            for (var i = 1; i <= ship.Size; i++)
+            foreach (var ship in myFleet)
             {
-               Console.WriteLine("Enter position {0} of {1} (i.e A3):", i, ship.Size);
-               ship.AddPosition(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("Please enter the positions for the {0} (size: {1})", ship.Name, ship.Size);
+                for (var i = 1; i <= ship.Size; i++)
+                {
+                    Console.WriteLine("Enter position {0} of {1} (i.e A3):", i, ship.Size);
+                    var input = Console.ReadLine();
+                    ship.AddPosition(input);
+                    TrackTelemetryEvent("PlacedShipPosition", "Position",input);
+                }
             }
-         }
-      }
+        }
 
-      private static void InitializeEnemyFleet()
-      {
-         enemyFleet = GameController.InitializeShips().ToList();
+        private static void InitializeEnemyFleet()
+        {
+            enemyFleet = GameController.InitializeShips().ToList();
 
-         enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 4 });
-         enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 5 });
-         enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 6 });
-         enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 7 });
-         enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 8 });
+            enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 4 });
+            enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 5 });
+            enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 6 });
+            enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 7 });
+            enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 8 });
 
-         enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 6 });
-         enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 7 });
-         enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 8 });
-         enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 9 });
+            enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 6 });
+            enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 7 });
+            enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 8 });
+            enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 9 });
 
-         enemyFleet[2].Positions.Add(new Position { Column = Letters.A, Row = 3 });
-         enemyFleet[2].Positions.Add(new Position { Column = Letters.B, Row = 3 });
-         enemyFleet[2].Positions.Add(new Position { Column = Letters.C, Row = 3 });
+            enemyFleet[2].Positions.Add(new Position { Column = Letters.A, Row = 3 });
+            enemyFleet[2].Positions.Add(new Position { Column = Letters.B, Row = 3 });
+            enemyFleet[2].Positions.Add(new Position { Column = Letters.C, Row = 3 });
 
-         enemyFleet[3].Positions.Add(new Position { Column = Letters.F, Row = 8 });
-         enemyFleet[3].Positions.Add(new Position { Column = Letters.G, Row = 8 });
-         enemyFleet[3].Positions.Add(new Position { Column = Letters.H, Row = 8 });
+            enemyFleet[3].Positions.Add(new Position { Column = Letters.F, Row = 8 });
+            enemyFleet[3].Positions.Add(new Position { Column = Letters.G, Row = 8 });
+            enemyFleet[3].Positions.Add(new Position { Column = Letters.H, Row = 8 });
 
-         enemyFleet[4].Positions.Add(new Position { Column = Letters.C, Row = 5 });
-         enemyFleet[4].Positions.Add(new Position { Column = Letters.C, Row = 6 });
-      }
-   }
+            enemyFleet[4].Positions.Add(new Position { Column = Letters.C, Row = 5 });
+            enemyFleet[4].Positions.Add(new Position { Column = Letters.C, Row = 6 });
+        }
+        private static void InitializeTelemetryClient()
+        {
+            TelemetryConfiguration config = TelemetryConfiguration.Active; // Reads ApplicationInsights.config file if present
+            telemetryClient = new TelemetryClient(config);
+        }
+
+        private static void TrackTelemetryEvent(string eventName)
+        {
+            telemetryClient.TrackEvent(eventName, CreatePropertiesDictionary(new Dictionary<string, string>()));
+        }
+
+        private static void TrackTelemetryEvent(string eventName, string propertyKey, string propertyValue)
+        {
+            telemetryClient.TrackEvent(eventName, CreatePropertiesDictionary(new Dictionary<string, string>() { { propertyKey, propertyValue } }));
+        }
+        private static void TrackTelemetryEvent(string eventName, Dictionary<string, string> properties)
+        {
+            telemetryClient.TrackEvent(eventName, CreatePropertiesDictionary(properties));
+        }
+
+        private static void TrackTelemetryException(Exception exception)
+        {
+            telemetryClient.TrackException(exception, CreatePropertiesDictionary(new Dictionary<string, string>()));
+        }
+
+        private static Dictionary<string, string> CreatePropertiesDictionary(Dictionary<string, string> properties)
+        {
+            properties.Add("Team", teamName);
+            return properties;
+        }
+    }
 }
